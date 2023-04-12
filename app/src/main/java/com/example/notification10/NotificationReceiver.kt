dc8db62e5d14ddb3cd2692f45ac7a97566a7f084
User <ilhomjonIbragimov1502@gmail.com>
2023-04-12 17:52:14 +0500

    Service and custom notification

---
+++ app/src/main/java/com/example/notification10/NotificationReceiver.kt
@@ -0,0 +1,34 @@
+package com.example.notification10
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.IBinder
+import android.provider.Settings
+
+
+class NotificationReceiver : Service() {
+    lateinit var mediaPlayer:MediaPlayer
+
+    override fun onCreate() {
+        super.onCreate()
+        mediaPlayer = MediaPlayer()
+        mediaPlayer.setDataSource(this, Settings.System.DEFAULT_RINGTONE_URI)
+        mediaPlayer.prepare()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (mediaPlayer.isPlaying){
+            mediaPlayer.pause()
+        }else{
+            mediaPlayer.start()
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+
+}
